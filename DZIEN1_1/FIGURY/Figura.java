3d0887d4f302a516f7d9c0b2b1972e54624500e3
albim72 <marcin.albiniak72@gmail.com>
2022-12-19 12:09:30 +0100

    Create Figura.java

---
+++ DZIEN1_1/FIGURY/Figura.java
@@ -0,0 +1,11 @@
+public abstract class Figura {
+    protected double a;
+    protected double b;
+
+    public Figura(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public abstract double policzPole();
+}
