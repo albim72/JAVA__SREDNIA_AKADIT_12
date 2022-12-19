30ad560867a6b343a504e86d65277f50ade165fd
albim72 <marcin.albiniak72@gmail.com>
2022-12-19 12:58:31 +0100

    Update Main.java

--- DZIEN1_1/FIGURY/Main.java
+++ DZIEN1_1/FIGURY/Main.java
@@ -2,6 +2,12 @@ public class Main {
     public static void main(String[] args) {
 
         Prostokat pr = new Prostokat(4.6,7.8);
-        System.out.printf("Pole prostokąta wynosi %f",pr.policzPole());
+        System.out.printf("Pole prostokąta wynosi %f\n",pr.policzPole());
+
+        Trojkat tr = new Trojkat(6.3,9.9);
+        System.out.printf("Pole trójkąta wynosi %f\n",tr.policzPole());
+        
+        Trapez trp = new Trapez(4.8,3.6,3.2);
+        System.out.printf("Pole trapezu wynosi %f\n",trp.policzPole());
     }
 }
