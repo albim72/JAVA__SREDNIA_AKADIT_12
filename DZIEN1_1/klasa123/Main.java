24faaafaaf5166c6def520ea7d0a8af1139db148
albim72 <marcin.albiniak72@gmail.com>
2022-12-19 11:17:16 +0100

    Update Main.java

--- DZIEN1_1/klasa123/Main.java
+++ DZIEN1_1/klasa123/Main.java
@@ -1,8 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("_____________________________________");
+
         PierwszaKlasa pk = new PierwszaKlasa(3,7);
         pk.print_ab();
 
+        System.out.println("_____________________________________");
+
+        DrugaKlasa dk = new DrugaKlasa(5,8,9);
+        dk.print_ab();
+        dk.print_abc();
+        System.out.println("suma a+b+c*faktor = " + dk.sumuj());
     }
 }
