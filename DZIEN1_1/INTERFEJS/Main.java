ee43c12e418ee41367f41335b95029fe58f0d73b
albim72 <marcin.albiniak72@gmail.com>
2022-12-19 13:44:56 +0100

    Update Main.java

--- DZIEN1_1/INTERFEJS/Main.java
+++ DZIEN1_1/INTERFEJS/Main.java
@@ -3,8 +3,10 @@ public class Main {
 
         Users usr = new Users();
         usr.info();
+        System.out.printf("Rok: %d\n",IEkstra.rok);
         usr.opis(2300,0.88);
         System.out.println(usr.user("Jan","Kot"));
+        usr.sekcja();
 
 
     }
