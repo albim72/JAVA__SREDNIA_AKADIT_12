b281b407706e1226f776d0dfcde1611d19d31b68
albim72 <marcin.albiniak72@gmail.com>
2022-12-19 12:50:17 +0100

    Create Main.java

---
+++ DZIEN1_1/FIGURY/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Prostokat pr = new Prostokat(4.6,7.8);
+        System.out.printf("Pole prostokąta wynosi %f",pr.policzPole());
+    }
+}
