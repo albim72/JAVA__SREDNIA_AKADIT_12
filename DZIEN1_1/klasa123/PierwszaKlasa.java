a1b9bd3bfbb819a57f3a950a3f6c45c75f28c9ca
albim72 <marcin.albiniak72@gmail.com>
2022-12-19 11:04:56 +0100

    Create PierwszaKlasa.java

---
+++ DZIEN1_1/klasa123/PierwszaKlasa.java
@@ -0,0 +1,13 @@
+public class PierwszaKlasa {
+    int a;
+    int b;
+
+    public PierwszaKlasa(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public void print_ab(){
+        System.out.println("Wartości: a = " + a + ", b = " + b);
+    }
+}
