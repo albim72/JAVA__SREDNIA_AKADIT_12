b55a9e41cec1fef1a46ec37330eede6a931bb99d
albim72 <marcin.albiniak72@gmail.com>
2022-12-19 13:49:55 +0100

    Create Otwieranie.java

---
+++ DZIEN1_1/NESTED/Otwieranie.java
@@ -0,0 +1,6 @@
+public class Otwieranie implements Pojemnik.Tak {
+    @Override
+    public void show() {
+        System.out.println("to jest otwieranie pojemnika... dostęp do interfeksu zgnieżdzonego wewnątrz klasy.");
+    }
+}
