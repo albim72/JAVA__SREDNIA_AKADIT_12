857c02c213b102abd5a34311ea6635caf0885159
albim72 <marcin.albiniak72@gmail.com>
2022-12-19 13:44:34 +0100

    Update Users.java

--- DZIEN1_1/INTERFEJS/Users.java
+++ DZIEN1_1/INTERFEJS/Users.java
@@ -1,4 +1,6 @@
 public class Users implements InterA,InterB{
+
+    int rok = 2090;
     @Override
     public void info() {
         System.out.println("Rejestracja: Konferencja IntelliAlg 2023");
@@ -16,12 +18,17 @@ public class Users implements InterA,InterB{
 
     @Override
     public void opis(int x, double y) {
-        System.out.printf("Kwota do zapłaty: %f zł",multi(x,y));
+        System.out.printf("Kwota do zapłaty: %4.2f zł\n",multi(x,y));
     }
 
     @Override
     public String user(String imie, String nazwisko) {
         return String.format("Uczestnik konferencji: %s %s, opłata wniesiona.",imie,nazwisko);
 
+    }
+
+    @Override
+    public void sekcja() {
+        System.out.println("sekcja nr 1: algorytmy AI");
     }
 }
