07d7de04ae490e11afe3c92c4ba1559f5d8c02c6
albim72 <marcin.albiniak72@gmail.com>
2022-12-19 11:16:29 +0100

    Update PierwszaKlasa.java

--- DZIEN1_1/klasa123/PierwszaKlasa.java
+++ DZIEN1_1/klasa123/PierwszaKlasa.java
@@ -1,10 +1,13 @@
 public class PierwszaKlasa {
     int a;
     int b;
+    int faktor;
 
     public PierwszaKlasa(int a, int b) {
         this.a = a;
         this.b = b;
+        this.faktor = 2;
+
     }
 
     public void print_ab(){
