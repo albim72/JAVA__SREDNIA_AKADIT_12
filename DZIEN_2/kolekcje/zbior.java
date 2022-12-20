1220e4b74ced410d8783dbf3340c0b39fe96d9d6
albim72 <marcin.albiniak72@gmail.com>
2022-12-20 13:08:45 +0100

    Create zbior.java

---
+++ DZIEN_2/kolekcje/zbior.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Set<String> kolory = new HashSet<>();
+        kolory.add("zielony");
+        kolory.add("biały");
+        kolory.add("czarny");
+        kolory.add("niebieski");
+        kolory.add("czerwony");
+        kolory.add("navy");
+        System.out.println(kolory);
+    }
+}
