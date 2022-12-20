da0c8e640e0434e7ba245d66948a53e045223b40
albim72 <marcin.albiniak72@gmail.com>
2022-12-20 14:09:59 +0100

    Create Twice.java

---
+++ DZIEN_2/generyki/twice_generic/Twice.java
@@ -0,0 +1,14 @@
+public class Twice<T,U> {
+    T obj1;
+    U obj2;
+
+    public Twice(T obj1, U obj2) {
+        this.obj1 = obj1;
+        this.obj2 = obj2;
+    }
+
+    public void print() {
+        System.out.println(obj1);
+        System.out.println(obj2);
+    }
+}
