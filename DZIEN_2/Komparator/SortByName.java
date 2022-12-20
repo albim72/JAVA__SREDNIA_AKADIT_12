df62493a20a6234d9210c639cff2140873e3b95a
albim72 <marcin.albiniak72@gmail.com>
2022-12-20 14:36:47 +0100

    Create SortByName.java

---
+++ DZIEN_2/Komparator/SortByName.java
@@ -0,0 +1,8 @@
+import java.util.Comparator;
+
+public class SortByName implements Comparator<Student> {
+    @Override
+    public int compare(Student a, Student b) {
+        return a.name.compareTo(b.name);
+    }
+}
