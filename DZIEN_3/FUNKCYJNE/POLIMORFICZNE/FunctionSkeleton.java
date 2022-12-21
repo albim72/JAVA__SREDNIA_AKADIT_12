c385527df5c02c9ac79b7604f8cf0500c79fae94
albim72 <marcin.albiniak72@gmail.com>
2022-12-21 12:26:02 +0100

    Create FunctionSkeleton.java

---
+++ DZIEN_3/FUNKCYJNE/POLIMORFICZNE/FunctionSkeleton.java
@@ -0,0 +1,23 @@
+public class FunctionExamples {
+
+  public static final Function<Integer, Integer> triple = new Function<Integer, Integer>() {
+
+    @Override
+    public Integer apply(Integer arg) {
+      return arg * 3;
+    }
+  };
+
+  public static final Function<Integer, Integer> square = new Function<Integer, Integer>() {
+
+    @Override
+    public Integer apply(Integer arg) {
+      return arg * arg;
+    }
+  };
+
+  public static final Function<Integer, Integer> compose(final Function<Integer, Integer> f1,
+                                                         final Function<Integer, Integer> f2) {
+    throw new RuntimeException("Do zaimplementowania");
+  }
+}
